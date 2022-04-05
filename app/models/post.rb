f062622cc64b3c727626f166a592534d95c9711d
Michael Dally <mddally@gmail.com>
2022-04-05 10:53:04 -0400

    commit tree 18387de48799454da7880332bdcd76228234b811

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction'] }
+
+    validate :clickbaity
+    
+    def clickbaity
+        if title
+            unless title.match?(/Won't Believe|Secret|Top \d+|Guess/i)
+                errors.add(:title, "That title just doesn't have the clicks")
+            end
+        end
+    end
 end
